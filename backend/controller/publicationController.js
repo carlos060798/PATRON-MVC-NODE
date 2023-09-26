01b2835da020579b2a2799da553021cefbafedea
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-09-26 13:48:06 -0500

    version final api clone de twiter basico con funciones basicas

--- backend/controller/publicationController.js
+++ backend/controller/publicationController.js
@@ -1,4 +1,205 @@
-// controladores de usuarios
+// Importar los módulos y dependencias necesarias
+import Publication from "../models/publicationModel.js";
+import mongoosePaginate from 'mongoose-paginate-v2';
+import { followUserIds } from "../services/followuserId.js";
+
+// Definir función para crear una nueva publicación
+const createPublic = async (req, res) => {
+    try {
+        // Recibir datos de la publicación desde el cuerpo de la solicitud
+        const params = req.body;
+
+        if (!params.text)
+            return res
+                .status(400)
+                .json({ message: "El texto de la publicación es requerido" });
+
+        // Crear un objeto del modelo de Publicación
+        const newpublication = new Publication(params);
+        // Asignar el usuario actual a la publicación
+        newpublication.user = req.user.id;
+        // Guardar la publicación en la base de datos
+        const publicationSaved = await newpublication.save();
+        // Devolver una respuesta
+        if (!publicationSaved)
+            return res
+                .status(200)
+                .json({ message: "No se pudo guardar la publicación" });
+        return res.status(200).json({
+            Msg: "Publicación creada",
+            publicationSaved,
+        });
+    } catch (error) {
+        console.log(error);
+        return res
+            .status(500)
+            .json({ message: "Error al guardar la publicación", error });
+    }
+};
+
+// Definir función para obtener los detalles de una publicación por su ID
+const getPublicdtail = async (req, res) => {
+    try {
+        // Recoger el ID de la publicación desde los parámetros de la solicitud
+        const publicId = req.params.id;
+
+        // Buscar la publicación en la base de datos por su ID
+        const publicDetail = await Publication.findById(publicId);
+
+        if (!publicDetail)
+            return res.status(404).json({ message: "Publicación no encontrada" });
+
+        // Devolver el resultado
+        return res.status(200).json({
+            message: "Publicación encontrada",
+            publicDetail,
+        });
+    } catch (error) {
+        console.log(error);
+        return res
+            .status(500)
+            .json({ message: "Error al obtener la publicación", error });
+    }
+};
+
+// Definir función para obtener las publicaciones de un usuario por su ID
+const getpublicUserMe = async (req, res) => {
+    try {
+        // Sacar el ID de usuario desde los parámetros de la solicitud
+        const userId = req.params.id;
+
+        // Controlar la página actual
+        let page = 1;
+
+        if (req.params.page) page = parseInt(req.params.page);
+
+        const itemsPerPage = 5;
+
+        // Opciones de paginación
+        const options = {
+            page,
+            limit: itemsPerPage,
+            sort: { created_at: -1 }, // Cambiar a "created_at" si ese es el campo de ordenamiento correcto
+        };
+
+        // Buscar publicaciones del usuario utilizando mongoose-paginate-v2
+        const query = { user: userId }; // Filtro de búsqueda
+        const result = await Publication.paginate(query, options);
+
+        if (!result.docs || result.docs.length <= 0) {
+            return res.status(404).json({
+                status: "error",
+                message: "No hay publicaciones para mostrar",
+            });
+        }
+
+        // Utilizar el método populate para obtener los datos del usuario
+        await Publication.populate(result.docs, { path: 'user', select: '-password -__v -role -email' });
+
+        return res.status(200).json({
+            status: "success",
+            message: "Publicaciones del perfil de un usuario",
+            page: result.page,
+            total: result.totalDocs,
+            pages: result.totalPages,
+            publications: result.docs,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            status: "error",
+            message: "Error al obtener publicaciones del usuario",
+            error: error.message,
+        });
+    }
+};
+
+// Definir función para eliminar una publicación por su ID
+const deletePublic = async (req, res) => {
+    try {
+        const publicId = req.params.id;
+
+        // Buscar la publicación por su ID y eliminarla
+        const deletePublic = await Publication.findByIdAndDelete(publicId);
+
+        if (!deletePublic) return res.status(404).json({ message: "Publicación no encontrada" });
+
+        // Devolver el resultado
+        return res.status(200).json({
+            message: "Publicación eliminada",
+            deletePublic
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ message: "Error al eliminar la publicación", e });
+    }
+};
+
+// Definir función para obtener el feed de publicaciones
+const publicFeed = async (req, res) => {
+     // Sacar la página actual
+     let page = 1;
+
+     if (req.params.page) {
+         page = req.params.page;
+     }
+ 
+     // Establecer número de elementos por página
+     const itemsPerPage = 5;
+ 
+     // Sacar un array de identificadores de usuarios a los que el usuario logueado sigue
+     try {
+         const myFollows = await followUserIds(req.user.id);
+ 
+         // Encontrar publicaciones de los usuarios seguidos, ordenarlas, popularlas y paginarlas
+         const query = Publication.find({ user: { $in: myFollows.following } })
+             .populate("user", "-password -role -__v -email")
+             .sort("-created_at");
+ 
+         const options = {
+             page: page,
+             limit: itemsPerPage,
+         };
+ 
+         const publications = await Publication.paginate(query, options);
+ 
+         if (!publications || publications.docs.length === 0) {
+             return res.status(500).send({
+                 status: "error",
+                 message: "No hay publicaciones para mostrar",
+             });
+         }
+ 
+         return res.status(200).send({
+             status: "success",
+             message: "Feed de publicaciones",
+             following: myFollows.following,
+             total: publications.totalDocs,
+             page: publications.page,
+             pages: publications.totalPages,
+             publications: publications.docs,
+         });
+     } catch (error) {
+         return res.status(500).send({
+             status: "error",
+             error,
+             message: "Error al obtener usuarios que sigues",
+         });
+     }
+};
+
+// Exportar las funciones para su uso en otras partes de la aplicación
+export {
+    createPublic,
+    getpublicUserMe,
+    deletePublic,
+    publicFeed,
+    getPublicdtail,
+};
+
+/*
+* en caso de que no sirva el codigo descomentar este
+/ controladores de las publicaciones de usuarios
 
 import Publication from "../models/publicationModel.js";
 import mongoosePaginate from 'mongoose-paginate-v2';
@@ -200,33 +401,6 @@ const publicFeed = async (req, res) => {
              message: "Error al obtener usuarios que sigues",
          });
      }
-/*
-    try{
-    // sacar  la pagina actual
-    let page = 1;
-    if (req.params.page) page = parseInt(req.params.page);
-
-    //  sacar un array de los usuarios identificados ques Sigo
-  let itemsPerPage = 5;
-     const myfollows = await followUserIds(req.user.id);
-
-    //  encontrar su publicaciones
-const publicationFollowing = await  Publication.find({user:{$in:myfollows.following}})
-
-    // usuarios
-
-    return res.json({
-        message: "feed de publicaciones",
-        myfollowstatus: myfollows.following,
-        publicationFollowing
-
-    })
-
-
-    } catch(err){
-        console.log(err);
-        return res.status(500).json({ message: "Error al obtener el feed", err });
-    }*/
 };
 
 export {
@@ -236,3 +410,4 @@ export {
     publicFeed,
     getPublicdtail,
 };
+*/
--- backend/routes/publicRoutes.js
+++ backend/routes/publicRoutes.js
@@ -1,4 +1,58 @@
-// rutas de la publisher
+// Importar los controladores de publicaciones y otras dependencias necesarias
+import {
+    createPublic,
+    getPublicdtail,
+    deletePublic,
+    getpublicUserMe,
+    publicFeed
+} from '../controller/publicationController.js';
+import { Router } from 'express';
+import auth from '../middlewares/auth.js';
+import multer from 'multer'; 
+import { uploadUserPublication,  viewPublication} from '../controller/seconds/DocumentController.js';
+
+// Configuración de la subida de archivos utilizando Multer
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        // Directorio de destino para las imágenes de publicaciones
+        cb(null, './uploads/publication');
+    },
+    filename: function(req, file, cb){
+        // Nombre de archivo para la imagen de la publicación
+        cb(null, "pub-" + Date.now() + file.originalname);
+    }
+});
+
+// Middleware para subir archivos utilizando Multer
+const uploads = multer({storage});
+
+// Crear una instancia de Router de Express
+const router = Router();
+
+// Definir las rutas y vincularlas a los controladores correspondientes
+router.post('/save', auth, createPublic); // Ruta para crear una nueva publicación
+router.get('/detail/:id', auth, getPublicdtail); // Ruta para obtener detalles de una publicación por su ID
+router.get('/user/:id/:page?', auth, getpublicUserMe); // Ruta para obtener las publicaciones de un usuario
+router.get('/publication/:file', viewPublication); // Ruta para ver una publicación específica
+router.get('/feed/:page?', auth, publicFeed); // Ruta para obtener el feed de publicaciones
+router.delete('/delete/:id', deletePublic); // Ruta para eliminar una publicación por su ID
+router.post('/upload/:id', [auth, uploads.single("file0")], uploadUserPublication); // Ruta para subir una imagen de publicación
+
+// Exportar las rutas configuradas
+const publicRoutes = router;
+
+export default publicRoutes;
+
+
+
+
+
+
+
+
+
+/* descomentar si no sirve este
+/ rutas de la publisher
 import {
     createPublic,
     getPublicdtail,
@@ -36,4 +90,4 @@ router.post('/upload/:id',[auth,uploads.single("file0")],uploadUserPublication);
 
 const publicRoutes = router
 
-export default publicRoutes;
+export default publicRoutes;*/
