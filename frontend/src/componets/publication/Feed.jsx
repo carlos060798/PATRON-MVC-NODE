52736d013b9ecda284f02bbc3245d102c43f7947
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-10-01 10:27:17 -0500

    version crud 1.6

--- frontend/src/componets/publication/Feed.jsx
+++ frontend/src/componets/publication/Feed.jsx
@@ -1,47 +1,86 @@
-import React, { useState } from 'react';
-import useAuth from '../../hooks/useAuth';
+import avatar from "../../assets/img/user.png";
 
-function  FeedPage() {
-  const{user}=useAuth();
+import React, { useState } from "react";
+import useAuth from "../../hooks/useAuth";
+
+function FeedPage() {
+  const { user } = useAuth();
   const [tweets, setTweets] = useState([
     {
       id: 1,
-      username: 'Usuario1',
-      time: 'Hace 1 hora',
-      text: 'Este es un tweet de ejemplo 1.',
+      username: "Usuario1",
+      time: "Hace 1 hora",
+      text: "Este es un tweet de ejemplo 1.",
+      isFollowed: false,
     },
     {
       id: 2,
-      username: 'Usuario2',
-      time: 'Hace 2 horas',
-      text: 'Este es un tweet de ejemplo 2.',
+      username: "Usuario2",
+      time: "Hace 2 horas",
+      text: "Este es un tweet de ejemplo 2.",
+      isFollowed: true,
     },
   ]);
 
-  const handleDeleteTweet = (id) => {
-    const updatedTweets = tweets.filter((tweet) => tweet.id !== id);
+  const handleToggleFollow = (id) => {
+    const updatedTweets = tweets.map((tweet) => {
+      if (tweet.id === id) {
+        return { ...tweet, isFollowed: !tweet.isFollowed };
+      }
+      return tweet;
+    });
     setTweets(updatedTweets);
   };
-  console.log(user);
+
+  const handleLoadMore = () => {
+    // Implementa lógica para cargar más tweets si es necesario
+  };
+
   return (
-    <div className="container mt-4">
-      {tweets.map((tweet) => (
-        <div className="card mb-3" key={tweet.id}>
-          <div className="card-body">
-            <div className="media">
-              <img src="imagen_usuario.jpg" className="mr-3 rounded-circle" alt="Imagen de Usuario" width="64" height="64" />
-              <div className="media-body">
-                <h5 className="mt-0">{tweet.username}</h5>
-                <p className="text-muted">{tweet.time}</p>
-                <p>{tweet.text}</p>
-                <button type="button" className="btn btn-danger" onClick={() => handleDeleteTweet(tweet.id)}>Eliminar</button>
-              </div>
+   <>
+   
+   <div className="container mt-4">
+      <h1>GENTE</h1>
+      <div className="card mb-3 d-flex p-4">
+        <div className="d-flex align-items-center mr-3">
+          <img src={avatar} className="rounded-circle mx-2" alt="Imagen de Usuario" width="64" height="64" />
+          <p className="text-muted mt-4 mx-1">Hace 1 hora</p>
+        </div>
+        <div className="d-flex flex-column w-100">
+          <div className="d-flex justify-content-between align-items-center">
+            <div>
+              <h5 className="mt-0">Usuario1</h5>
+              <p>Este es un tweet de ejemplo 1.</p>
+            </div>
+            <div className="d-flex">
+              <button type="button" className="btn btn-primary btn-sm mx-2">
+                <i className="fas fa-thumbs-up"></i> Like
+              </button>
+              <button type="button" className="btn btn-danger btn-sm">
+                <i className="fas fa-thumbs-down"></i> Dislike
+              </button>
             </div>
           </div>
         </div>
-      ))}
+      </div>
+      <div className="d-flex justify-content-center mt-3">
+        <button type="button" className="btn btn-primary">
+          Ver más publicaciones
+        </button>
+      </div>
     </div>
+   </>
   );
 }
 
 export default FeedPage;
+
+
+
+
+
+
+
+
+
+
