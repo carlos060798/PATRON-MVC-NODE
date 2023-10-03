17857b5abc019e21f66e35cc939a67af53554bb6
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-10-02 21:09:28 -0500

    v.1.3 de publicacion

--- frontend/src/componets/layaout/Private/Sibebar.jsx
+++ frontend/src/componets/layaout/Private/Sibebar.jsx
@@ -8,9 +8,7 @@ function SideNavbar() {
   const { user, counter } = useAuth();
   let token = localStorage.getItem("token");
 
-  const [formData, setFormData] = useState({
-   
-  });
+  const [formData, setFormData] = useState({});
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -20,42 +18,48 @@ function SideNavbar() {
     }));
   };
 
-  const CreatePublication = async (e ) => {
-
+  const CreatePublication = async (e) => {
     e.preventDefault();
-  
+
     let nuwData = formData;
     nuwData.user = user._id;
     const config = {
       headers: {
         "Content-Type": "application/json",
         Authorization: token,
-      }
-    }
+      },
+    };
 
-    const request = await axios.post("http://localhost:4100/api/public/save",nuwData,config);
-    const{data,status} = request;
+    const request = await axios.post(
+      "http://localhost:4100/api/public/save",
+      nuwData,
+      config
+    );
+    const { data, status } = request;
     const publicationid = data.publicationSaved._id;
-    const  fileinput = document.querySelector("#file");
+    const fileinput = document.querySelector("#file");
 
-    if (status === 200 && fileinput.files[0]) {   
+    if (status === 200 && fileinput.files[0]) {
       const formData = new FormData();
-      formData.append("file0",fileinput.files[0]);
+      formData.append("file0", fileinput.files[0]);
       const config2 = {
         headers: {
           "Content-Type": "multipart/form-data",
           Authorization: token,
         },
-      }
+      };
       const responseImg = await axios.post(
         `http://localhost:4100/api/public/upload/${publicationid}`,
         formData,
         config2
       );
       console.log(responseImg);
-    
-  }
-
+      if (status === 200 && responseImg.status === 200) {
+        const formPublication = document
+          .querySelector("#publicationform")
+          .reset();
+      }
+    }
   };
   return (
     <div className="container mt-4">
@@ -110,7 +114,7 @@ function SideNavbar() {
         <div className="row">
           <div className="card">
             <div className="card-body">
-              <form onSubmit={CreatePublication}>
+              <form id="publicationform" onSubmit={CreatePublication}>
                 <div className="mb-3">
                   <textarea
                     className="form-control"
