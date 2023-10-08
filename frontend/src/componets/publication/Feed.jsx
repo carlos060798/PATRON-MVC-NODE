f41887e7e56e4be38d8b28149459eddcd6bd7a5e
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-10-08 09:08:28 -0500

    mejora de publicacion

--- frontend/src/componets/publication/Feed.jsx
+++ frontend/src/componets/publication/Feed.jsx
@@ -1,110 +1,112 @@
 import usePublication from "../../hooks/usePublication";
 import avatar from "../../assets/img/user.png";
 import formatearFecha from "../../helpers/formatofecha";
+import {uid as unicode} from 'uid'
 
 function FeedPage() {
-const{
-  formData,
-  handleChange,
-  CreatePublication,
-  alert,
-  publics,
-  newPage,
-}=usePublication();
-return (
-  <div className="container mt-4">
+  const { formData, handleChange, CreatePublication, alert, publics, newPage,hasMorePublications } =
+    usePublication();
+    console.log(hasMorePublications)
+  return (
+    <div className="container mt-4">
       <h1 className="text-center fw-bold text-dark">Publicaciones</h1>
-    <div className="col">
-      <div className="row justify-content-center align-items-center">
-        <div className="card col-lg-7 col-md-10 col-12">
-          <div className="card-body">
-            <form id="publicationform" onSubmit={CreatePublication}>
-              <div className="mb-3">
-                <textarea
-                  className="form-control"
-                  placeholder="¿Qué estás pensando?"
-                  rows="3"
-                  name="text"
-                  value={formData.text || ""}
-                  onChange={handleChange}
-                ></textarea>
-              </div>
-              <div className="mb-3">
-                <label htmlFor="file0" className="form-label">
-                  Subir imagen
-                </label>
-                <input
-                  type="file"
-                  id="file"
-                  className="form-control"
-                  name="file0"
-                  onChange={handleChange}
-                />
-              </div>
-              <button type="submit" className="btn btn-primary">
-                <i className="fas fa-pen"></i> 
-              </button>
-            </form>
-           
-            {alert.type === "success" && (
-              <div className="alert alert-success mt-4" role="alert">
-                {alert.message}
-              </div>
-            )}
-            {alert.type === "error" && (
-              <div className="alert alert-danger mt-4" role="alert">
-                {alert.message}
-              </div>
-            )}
+      <div className="col">
+        <div className="row justify-content-center align-items-center">
+          <div className="card col-lg-7 col-md-10 col-12">
+            <div className="card-body">
+              <form id="publicationform" onSubmit={CreatePublication}>
+                <div className="mb-3">
+                  <textarea
+                    className="form-control"
+                    placeholder="¿Qué estás pensando?"
+                    rows="3"
+                    name="text"
+                    value={formData.text || ""}
+                    onChange={handleChange}
+                  ></textarea>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="file0" className="form-label">
+                    Subir imagen
+                  </label>
+                  <input
+                    type="file"
+                    id="file"
+                    className="form-control"
+                    name="file0"
+                    onChange={handleChange}
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  <i className="fas fa-pen"></i>
+                </button>
+              </form>
+
+              {alert.type === "success" && (
+                <div className="alert alert-success mt-4" role="alert">
+                  {alert.message}
+                </div>
+              )}
+              {alert.type === "error" && (
+                <div className="alert alert-danger mt-4" role="alert">
+                  {alert.message}
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="row mt-4 justify-content-center">
-        <div className="col-lg-7 col-md-10 col-12">
-          {publics.map((publicacion) => (
-            <div className="card mb-3" key={publicacion._id}>
-              <div className="card-body">
-                <div className=" align-items-center">
-                <div className="d-flex">
-                  <img
-                    src={avatar}
-                    className="rounded-circle mr-3"
-                    alt="Imagen de Usuario"
-                    width="64"
-                    height="64"
-                  />
-                  
-                    <h5  className="mx-3 my-2">
-                      {publicacion.user.name} {publicacion.user.surname}
-                    </h5>
-                    <h5  className=" my-2">{formatearFecha(publicacion.created_at)}</h5>
+        <div className="row mt-4 justify-content-center">
+          <div className="col-lg-7 col-md-10 col-12">
+            {publics.map((publicacion) => (
+              <div className="card mb-3" key={unicode()}>
+                <div className="card-body">
+                  <div className=" align-items-center">
+                    <div className="d-flex">
+                      <img
+                        src={avatar}
+                        className="rounded-circle mr-3"
+                        alt="Imagen de Usuario"
+                        width="64"
+                        height="64"
+                      />
+
+                      <h5 className="mx-3 my-2">
+                        {publicacion.user.name} {publicacion.user.surname}
+                      </h5>
+                      <h5 className=" my-2">
+                        {formatearFecha(publicacion.created_at)}
+                      </h5>
                     </div>
-                    
+
                     <p>{publicacion.text}</p>
                     {publicacion.file && (
-                     <img
-                     src={`http://localhost:4100/api/public/publication/${publicacion.file}`}
-                     alt=""
-                     className="img-fluid mx-auto d-block"
-                     style={{ Height: "300px", Width: "300px" }}
-                   />
+                      <img
+                        src={`http://localhost:4100/api/public/publication/${publicacion.file}`}
+                        alt=""
+                        className="img-fluid mx-auto d-block"
+                        style={{ Height: "300px", Width: "300px" }}
+                      />
                     )}
-                    
-              
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-          <div className="text-center">
-            <button type="button" className="btn btn-primary" onClick={newPage}>
-              Ver más publicaciones
-            </button>
+            ))}
+            {hasMorePublications && (
+              <div className="d-flex justify-content-center">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => newPage()}
+                >
+                  Ver más publicaciones
+                </button>
+              </div>
+            )}
+             
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default FeedPage;
--- frontend/src/hooks/usePublication.jsx
+++ frontend/src/hooks/usePublication.jsx
@@ -8,7 +8,9 @@ const usePublication = () => {
   const [publics, setPublics] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const  [hasMorePublications, setHasMorePublications] = useState(true); // Nuevo estado para controlar si hay más publicaciones
   const { user } = useAuth();
+
 
   useEffect(() => {
     getPublications();
@@ -17,7 +19,7 @@ const usePublication = () => {
   const getPublications = async () => {
     try {
       setLoading(true);
-
+  
       let token = localStorage.getItem("token");
       const config = {
         headers: {
@@ -25,20 +27,28 @@ const usePublication = () => {
           Authorization: token,
         },
       };
-
+  
       const request = await axios.get(
         `http://localhost:4100/api/public/feed/${page}`,
         config
       );
-
+  
       const { data, status } = request;
       const { publications } = data;
-
+  
       if (status === 200) {
         if (page === 1) {
+          // Si es la primera página, establece las publicaciones directamente
           setPublics(publications);
         } else {
-          setPublics([...publics, ...publications]);
+          // Si no es la primera página, verifica si hay nuevas publicaciones
+          if (publications.length === 0) {
+            // No hay más publicaciones, desactiva la carga adicional
+            setHasMorePublications(false);
+          } else {
+            // Hay nuevas publicaciones, agrégales a la lista existente
+            setPublics((prevPublics) => [...prevPublics, ...publications]);
+          }
         }
       }
     } catch (error) {
@@ -142,82 +152,9 @@ const usePublication = () => {
       setLoading(false);
     }
   };
- /* const CreatePublication = async (e) => {
-    try {
-      e.preventDefault();
-      setLoading(true);
 
-      const newData = {
-        ...formData,
-        user: user._id,
-      };
 
-      const token = localStorage.getItem("token");
-
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: token,
-        },
-      };
-
-      const request = await axios.post(
-        "http://localhost:4100/api/public/save",
-        newData,
-        config
-      );
-
-      const { data, status } = request;
-      const publicationId = data.publicationSaved._id;
-
-      const fileInput = document.querySelector("#file");
-
-      if (status === 200 && fileInput.files[0]) {
-        const formData = new FormData();
-        formData.append("file0", fileInput.files[0]);
-
-        const config2 = {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            Authorization: token,
-          },
-        };
-
-        const responseImg = await axios.post(
-          `http://localhost:4100/api/public/upload/${publicationId}`,
-          formData,
-          config2
-        );
-
-        if (responseImg.status === 200) {
-          // Limpiar el formulario después de la publicación exitosa
-          resetForm();
-
-          // Mostrar una alerta de éxito
-          setAlert({ type: "success", message: "Publicación creada con éxito" });
-
-          // Limpiar la alerta después de 4 segundos
-          setTimeout(() => {
-            window.location.reload();
-          }, 4000);
-        }
-         
-        else {
-          // Mostrar una alerta de error si la carga de la imagen falla
-          setAlert({ type: "error", message: "Error al subir la imagen" });
-        }
-      
-       
-      }  }catch (error) {
-      console.error("Error al crear la publicación:", error);
-      // Mostrar una alerta de error
-      setAlert({ type: "error", message: "Error al crear la publicación" });
-    } finally {
-      setLoading(false);
-    }
-  };*/
-
-  return { formData, handleChange, CreatePublication, alert, publics, newPage, loading };
+  return { formData, handleChange, CreatePublication, alert, publics, newPage, loading,hasMorePublications };
 };
 
 export default usePublication;
